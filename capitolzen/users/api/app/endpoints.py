6d48fdb7df19e6d302c90fca1c80bbd6ed26fc16
Zak Schlemmer <zak.schlemmer@gmail.com>
2017-07-04 08:16:45 -0400

    switch lookup_field to use id, beginning of detail_route for alert dismiss

--- capitolzen/users/api/app/endpoints.py
+++ capitolzen/users/api/app/endpoints.py
@@ -2,7 +2,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from dry_rest_permissions.generics import DRYPermissionFiltersBase
 from dry_rest_permissions.generics import DRYPermissions
 from rest_framework import viewsets
-from rest_framework.decorators import list_route
+from rest_framework.decorators import list_route, detail_route
 from rest_framework.exceptions import NotAuthenticated
 from rest_framework.response import Response
 
@@ -11,6 +11,7 @@ from capitolzen.users.api.app.serializers import UserSerializer
 from capitolzen.users.models import User
 from capitolzen.users.api.app.serializers import AlertsSerializer
 from capitolzen.users.models import Alerts
+from rest_framework import status
 
 
 class UserFilterBackend(DRYPermissionFiltersBase):
@@ -65,31 +66,12 @@ class UserViewSet(viewsets.ModelViewSet):
 
 
 class AlertsFilterBackend(DRYPermissionFiltersBase):
-    """
-    Filters the users to ensure users are now shown to other users who
-    shouldn't see them.
-    """
-
     def filter_list_queryset(self, request, queryset, view):
         queryset = Alerts.objects.filter(user=request.user)
         return queryset
 
 
 class AlertsViewSet(viewsets.ModelViewSet):
-    """
-    Override the various list and detail views to ensure that this endpoint
-    is being filtered by an organization and the user making the request is
-    an admin of the specific organization being filtered.
-    """
-
-    """
-    Override the list view for now to filter users down by org.
-
-    TODO:
-    -- Figure out a better way to do this.
-    -- Require an organization filter to exist. There is no reason people
-        should be attempting to view all the users.
-    """
     def list(self, request, *args, **kwargs):
         if "alerts" in request._request.GET:
             # = request._request.GET['alerts']
@@ -103,9 +85,16 @@ class AlertsViewSet(viewsets.ModelViewSet):
     def current(self, request):
         return Response(AlertsSerializer(request.user).data)
 
+    @detail_route(methods=['POST'])
+    def dismiss(self, request):
+        serializer = AlertsSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     permission_classes = (DRYPermissions, )
     serializer_class = AlertsSerializer
     queryset = Alerts.objects.all()
     filter_backends = (AlertsFilterBackend, DjangoFilterBackend)
-    lookup_field = "message"
+    lookup_field = "id"
 
