2b6212852eb87e0aab6d747642aced3fba9d407c
Donald Wasserman <djwasserman@gmail.com>
2017-07-06 12:03:33 -0400

    updated bill gathering

--- capitolzen/meta/states.py
+++ capitolzen/meta/states.py
@@ -1,20 +1,35 @@
+from collections import namedtuple
+
 AvailableStateChoices = (
     ('MI', 'Michigan'),
     ('OH', "Ohio")
 )
 
+StateData = namedtuple('StateData',
+                       'name title lower_bill_prefix upper_bill_prefix lower_bill_start upper_bill_start is_active'
+                       )
 
-AVAILABLE_STATES = [
-    {
-        "id": "MI",
-        "title": "Michigan",
-        "lower_bill_prefix": "HB",
-        "upper_bill_prefix": "SB",
-        "lower_bill_start": "4001",
-        "upper_bill_start": "0001",
-        "is_active": True
-    }
-]
+mi = StateData(
+    name='MI',
+    title="Michigan",
+    lower_bill_prefix='HB',
+    upper_bill_prefix='SB',
+    lower_bill_start='4001',
+    upper_bill_start='0001',
+    is_active=True
+)
+
+AVAILABLE_STATES = [mi]
+
+# {
+#     "id": "MI",
+#     "title": "Michigan",
+#     "lower_bill_prefix": "HB",
+#     "upper_bill_prefix": "SB",
+#     "lower_bill_start": "4001",
+#     "upper_bill_start": "0001",
+#     "is_active": True
+# }
 
 
 
