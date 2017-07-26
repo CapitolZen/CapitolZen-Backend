3a63aa5bff3c4138eccf407fe881913ca4b3bc7e
Donald Wasserman <djwasserman@gmail.com>
2017-07-26 05:34:50 -0400

    fixed typo

--- capitolzen/proposals/api/app/router.py
+++ capitolzen/proposals/api/app/router.py
@@ -5,7 +5,7 @@ router = routers.SimpleRouter()
 
 router.register(r'bills', BillViewSet, base_name="bills")
 router.register(r'legislators', LegislatorViewSet, base_name="legislators")
-router.register(r'commmittees', CommitteeViewSet, base_name="committees")
+router.register(r'committees', CommitteeViewSet, base_name="committees")
 router.register(r'wrappers', WrapperViewSet, base_name="wrappers")
 
 urlpatterns = router.urls
