b29ab1fe32da73879b19e468ab74a847dbb8898b
Donald Wasserman <djwasserman@gmail.com>
2017-09-21 14:44:00 -0400

    update deps to fix failing tests

--- capitolzen/proposals/api/app/endpoints.py
+++ capitolzen/proposals/api/app/endpoints.py
@@ -1,17 +1,23 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, filters
 from rest_framework.permissions import IsAuthenticated
-
+import rest_framework_filters as drffilter
 from dry_rest_permissions.generics import (DRYPermissions,
                                            DRYPermissionFiltersBase)
 from capitolzen.proposals.models import Bill, Wrapper, Legislator, Committee
 from .serializers import BillSerializer, WrapperSerializer, LegislatorSerializer, CommitteeSerializer
 
 
-class BillFilterBackend(DRYPermissionFiltersBase):
-    def filter_list_queryset(self, request, queryset, view):
-        # TODO: Add in org/state availability filtering
-        return queryset
+class BillFilter(drffilter.FilterSet):
+    # sponsor = drffilter.RelatedFilter(LegislatorFilter, name='sponsor', queryset=Legislator.objects.all())
+
+    class Meta:
+        model = Bill
+        fields = {
+            'state': ['exact', 'contains'],
+            'state_id': ['exact', 'contains'],
+            'title': ['contains', 'exact'],
+        }
 
 
 class BillViewSet(viewsets.ReadOnlyModelViewSet):
@@ -21,9 +27,20 @@ class BillViewSet(viewsets.ReadOnlyModelViewSet):
     permission_classes = (IsAuthenticated,)
     serializer_class = BillSerializer
     queryset = Bill.objects.all()
-    filter_backends = (BillFilterBackend, DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter)
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter)
+    filter_class = BillFilter
     ordering_fields = ('last_action_date', 'state', 'state_id',)
     search_fields = ('title', 'sponsor__last_name', 'sponsor__first_name', 'state_id')
+
+
+class LegislatorFilter(drffilter.FilterSet):
+    class Meta:
+        model = Legislator
+        fields = {
+            'first_name': ['exact', 'contains'],
+            'last_name': ['exact', 'contains'],
+            'party': ['exact'],
+        }
 
 
 class LegislatorViewSet(viewsets.ReadOnlyModelViewSet):
@@ -33,6 +50,7 @@ class LegislatorViewSet(viewsets.ReadOnlyModelViewSet):
     permission_classes = (IsAuthenticated, )
     serializer_class = LegislatorSerializer
     queryset = Legislator.objects.all()
+    filter_class = LegislatorFilter
     filter_backends = (DjangoFilterBackend, filters.SearchFilter)
 
 
@@ -45,10 +63,18 @@ class CommitteeViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Committee.objects.all()
 
 
-class WrapperFilter(filters.FilterSet):
+class WrapperFilter(drffilter.FilterSet):
+    bill = drffilter.RelatedFilter(BillFilter, name='bill', queryset=Bill.objects.all())
+
     class Meta:
         model = Wrapper
-        fields = ['bill__state', 'bill__state_id', 'bill__id', 'organization', 'group']
+        fields = {
+                    'organization': ['exact'],
+                    'group': ['in'],
+                    'position': ['exact'],
+                    'summary': ['in'],
+                    'starred': ['exact']
+                 }
 
 
 class WrapperViewSet(viewsets.ModelViewSet):
