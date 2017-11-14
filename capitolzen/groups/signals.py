643f12b9d4a725222fd47142f7f761ca77d9679d
Donald Wasserman <djwasserman@gmail.com>
2017-11-14 13:07:09 -0500

    backend fixes for committees

--- capitolzen/groups/signals.py
+++ capitolzen/groups/signals.py
@@ -1,22 +1,24 @@
-# from django.db.models.signals import post_save, post_delete
-# from django.dispatch import receiver
-# from .models import Group, Report
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from .models import Group, Report
 
-#
-# @receiver(post_save, sender=Group)
-# def create_group_default_report(sender, **kwargs):
-#     group = kwargs.get('instance')
-#     organization = group.organization
-#     user = organization.owner_user_account()
-#     preferences = {
-#         'layout': 'detail_list'
-#     }
-#     report = Report.objects.create(
-#         user=user,
-#         group=group,
-#         organization=organization,
-#         title="All Saved Bills",
-#         preferences=preferences
-#     )
-#
-#     report.save()
+
+@receiver(post_save, sender=Group)
+def create_group_default_report(sender, **kwargs):
+    group = kwargs.get('instance')
+    organization = group.organization
+
+    if getattr(organization, 'owner', False):
+        user = organization.owner_user_account()
+        preferences = {
+            'layout': 'detail_list'
+        }
+        report = Report.objects.create(
+            user=user,
+            group=group,
+            organization=organization,
+            title="All Saved Bills",
+            preferences=preferences
+        )
+
+        report.save()
