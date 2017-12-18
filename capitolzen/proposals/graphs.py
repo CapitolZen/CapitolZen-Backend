301f056cdc650364c18e270f2b2ba76f1106c309
Devon Bleibtrey <bleib1dj@gmail.com>
2017-12-18 11:30:00 -0500

    Working on finding related articles to analyze

--- capitolzen/proposals/graphs.py
+++ capitolzen/proposals/graphs.py
@@ -1,8 +1,17 @@
 from py2neo import Graph
+from textblob import TextBlob
+from rake_nltk import Rake
+import operator
+from gensim import corpora, models, similarities
+
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from elasticsearch_dsl import Search
+
 from django.conf import settings
 
 from capitolzen.proposals.models import Bill, Legislator
-
+from capitolzen.proposals.utils import tf, tfidf, idf
+from capitolzen.proposals.documents import BillDocument
 
 class BasicGraph(object):
     graph = Graph(**settings.GRAPH_DATABASE)
@@ -46,7 +55,10 @@ class BillGraph(BasicGraph):
         query = """
         MATCH (bill:Bill {uuid: $bill_uuid}), 
             (legislator:Legislator {remote_id: $remote_id})
-        MERGE (bill)<-[r:SPONSOR {type: $type}]-(legislator)
+        MERGE (bill)<-[r:SPONSOR {
+            type: $type
+            human_weight: $human_weight
+        }]-(legislator)
         RETURN bill, legislator
         """
         for sponsor in self.instance.sponsors:
@@ -57,11 +69,50 @@ class BillGraph(BasicGraph):
 
             if response:
                 # Now that we know that they exist link them to the bill
+                if sponsor.get('type') == "primary":
+                    human_weight = 2
+                else:
+                    human_weight = 1
                 self.graph.data(query, parameters={
                     "remote_id": sponsor.get('leg_id'),
                     "bill_uuid": str(self.instance.id),
-                    "type": sponsor.get('type')
+                    "type": sponsor.get('type'),
+                    "human_weight": human_weight
                 })
+
+    def generate_similarity_score(self):
+        # Find words of most interest / value in bill
+        # TODO: Might be able to simplify / improve results by using ES's
+        # aggregation & significant_terms capabilities.
+        es_client = Elasticsearch(
+            hosts=settings.ELASTICSEARCH_DSL['default']['hosts'],
+            connection_class=RequestsHttpConnection
+        )
+        r = Rake()
+        r.extract_keywords_from_text(self.instance.content)
+        key_phrases = [keyphrase[1] for keyphrase in r.get_ranked_phrases()[:3]]
+        for phrase in key_phrases:
+            s = Search().using(es_client).query('match', content=phrase)
+            s.doc_type(BillDocument)
+            response = s.execute()
+            for hit in response:
+                print(hit.id)
+        doc_list = []
+        scores = {word: tfidf(
+            word, self.instance.content, doc_list
+        ) for word in self.instance.content.words}
+        sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+        for word, score in sorted_words[:3]:
+            print("\tWord: {}, TF-IDF: {}".format(word, round(score, 5)))
+
+        # Search for other bills with similar keywords
+
+        # Determine the similarity of the given documents
+
+        # Plot scores on relationships between bills
+        query = """
+        MATCH (bill:Bill) 
+        """
 
     def run(self):
         self.merge()
