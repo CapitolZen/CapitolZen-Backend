3358f29e412f449044b67ea602c3a9846cac3404
Matt Wisner <matt@detroitweb.company>
2017-12-22 07:48:12 -0500

    Risk no stripe accounts being setup but fix duplicate stripe account creation

--- capitolzen/users/tasks.py
+++ capitolzen/users/tasks.py
@@ -12,7 +12,6 @@ from capitolzen.users.utils import get_intercom_client
 from celery.utils.log import get_task_logger
 logger = get_task_logger(__name__)
 
-
 @shared_task()
 def intercom_manage_user(user_id, operation):
     """
