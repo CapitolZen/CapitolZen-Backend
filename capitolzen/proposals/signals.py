a676fcc5335dcad39e9807a2d479f698d58b5646
Donald Wasserman <djwasserman@gmail.com>
2018-01-04 19:29:18 -0500

    stashing changes

--- capitolzen/proposals/signals.py
+++ capitolzen/proposals/signals.py
@@ -1,5 +1,6 @@
 from django.db.models.signals import post_save
 from django.db import transaction
+from django.db.models import Q
 
 from django.dispatch import receiver
 
@@ -17,11 +18,11 @@ def create_introduction_actions(sender, **kwargs):
     bill = kwargs.get('instance')
     created = kwargs.get('created')
     if created:
-        transaction.on_commit(lambda: create_actions(bill))
+        transaction.on_commit(lambda: _create_introduction_actions(bill))
 
 
-def create_actions(bill):
-    for user in User.objects.all():
+def _create_introduction_actions(bill):
+    for user in User.objects.filter(is_active=True, notification_preferences__bill__introduced=True):
         a = Action.objects.create(
             user=user,
             action_object=bill,
@@ -30,3 +31,19 @@ def create_actions(bill):
         )
 
         a.save()
+
+
+@receiver(post_save, sender=Event)
+def create_committee_actions(sender, **kwargs):
+    event = kwargs.get('instance')
+    created = kwargs.get('created')
+    if created:
+        transaction.on_commit()
+
+
+def _create_committee_actions(event):
+    users = User.objects.filter(
+        Q(is_active=True),
+        Q(notification_preferences__committee_meeting=['all']) |
+        Q(notification_preferences__committee_meeting__contains=event.committee.id)
+    )
